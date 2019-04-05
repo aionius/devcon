5d15177ae7d1003ab02cc0587d7d87ce600b0929
Aionius Dactlys <aionius@gmail.com>
2019-04-05 16:22:53 -0400

    Started coding profile page

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 
 import TextFieldGroup from "../common/TextFieldGroup";
+import TextAreaFieldGroup from "../common/TextAreaFieldGroup";
+import InputGroup from "../common/InputGroup";
+import SelectListGroup from "../common/SelectListGroup";
 
 function CreateProfile(props) {
   const [displaySocialInputs, setDisplaySocialInputs] = useState(false);
@@ -21,6 +24,149 @@ function CreateProfile(props) {
   const [instagram, setInstagram] = useState("");
   const [errors, setErrors] = useState({});
 
+  function onChangeEvent(event) {
+    const formName = event.target.name;
+    switch (formName) {
+      case "handle":
+        setHandle(event.target.value);
+        break;
+      case "company":
+        setCompany(event.target.value);
+        break;
+      case "website":
+        setWebsite(event.target.value);
+        break;
+      case "location":
+        setLocation(event.target.value);
+        break;
+      case "status":
+        setStatus(event.target.value);
+        break;
+      case "skills":
+        setSkills(event.target.value);
+        break;
+      case "githubusername":
+        setGithubusername(event.target.value);
+        break;
+      case "bio":
+        setBio(event.target.value);
+        break;
+      case "twitter":
+        setTwitter(event.target.value);
+        break;
+      case "facebook":
+        setFacebook(event.target.value);
+        break;
+      case "linkedin":
+        setLinkedin(event.target.value);
+        break;
+      case "youtube":
+        setYoutube(event.target.value);
+        break;
+      case "instagram":
+        setInstagram(event.target.value);
+        break;
+      default:
+        break;
+    }
+  }
+
+  function onSubmit(event) {
+    event.preventDefault();
+  }
+
+  useEffect(() => {
+    if (props.errors) {
+      setErrors(props.errors);
+    }
+  });
+
+  const options = [
+    {
+      label: "* Select Professional Status",
+      value: 0
+    },
+    {
+      label: "Developer",
+      value: "Developer"
+    },
+    {
+      label: "Junior Developer",
+      value: "Junior Developer"
+    },
+    {
+      label: "Senior Developer",
+      value: "Senior Developer"
+    },
+    {
+      label: "Manager",
+      value: "Manager"
+    },
+    {
+      label: "Student or Learning",
+      value: "Student or Learning"
+    },
+    {
+      label: "Instructor or Teacher",
+      value: "Instructor or Teacher"
+    },
+    {
+      label: "Intern",
+      value: "Intern"
+    },
+    {
+      label: "Other",
+      value: "Other"
+    }
+  ];
+
+  let socialInputs;
+  if (displaySocialInputs) {
+    socialInputs = (
+      <div>
+        <InputGroup
+          placeholder="Twitter"
+          name="twitter"
+          icon="fab fa-twitter"
+          value={twitter}
+          onChange={onChangeEvent}
+          error={errors.twitter}
+        />
+        <InputGroup
+          placeholder="Facebook"
+          name="facebook"
+          icon="fab fa-facebook"
+          value={facebook}
+          onChange={onChangeEvent}
+          error={errors.facebook}
+        />
+        <InputGroup
+          placeholder="Linkedin"
+          name="linkedin"
+          icon="fab fa-linkedin"
+          value={linkedin}
+          onChange={onChangeEvent}
+          error={errors.linkedin}
+        />
+        <InputGroup
+          placeholder="Youtube"
+          name="youtube"
+          icon="fab fa-youtube"
+          value={youtube}
+          onChange={onChangeEvent}
+          error={errors.youtube}
+        />
+        <InputGroup
+          placeholder="Instagram"
+          name="instagram"
+          icon="fab fa-instagram"
+          value={instagram}
+          onChange={onChangeEvent}
+          error={errors.instagram}
+        />
+      </div>
+    );
+  }
   return (
     <div className="create-profile">
       <div className="container">
@@ -31,6 +177,90 @@ function CreateProfile(props) {
               Let's get some information to make your profile stand out
             </p>
             <small className="d-block pb-3">* = required fields</small>
+            <form onSubmit={onSubmit}>
+              <TextFieldGroup
+                placeholder="* Profile Handle"
+                name="handle"
+                value={handle}
+                onChange={onChangeEvent}
+                error={errors.handle}
+                info="A unique handle for your profile URL. Your full name, company name, nickname."
+              />
+              <SelectListGroup
+                placeholder="Status"
+                name="status"
+                value={status}
+                onChange={onChangeEvent}
+                options={options}
+                error={errors.status}
+                info="Give us an idea of where you are at in your career."
+              />
+              <TextFieldGroup
+                placeholder="Company"
+                name="company"
+                value={company}
+                onChange={onChangeEvent}
+                error={errors.company}
+                info="Could be your own company or one you work for."
+              />
+              <TextFieldGroup
+                placeholder="Website"
+                name="website"
+                value={website}
+                onChange={onChangeEvent}
+                error={errors.website}
+                info="Could be your own or a company website."
+              />
+              <TextFieldGroup
+                placeholder="Location"
+                name="location"
+                value={location}
+                onChange={onChangeEvent}
+                error={errors.location}
+                info="City & state suggested (eg. Boston, MA)."
+              />
+              <TextFieldGroup
+                placeholder="Skills"
+                name="skills"
+                value={skills}
+                onChange={onChangeEvent}
+                error={errors.skills}
+                info="Please use comma separated values (eg. HTML,CSS,JavaScript,PHP)."
+              />
+              <TextFieldGroup
+                placeholder="Github Username"
+                name="githubusername"
+                value={githubusername}
+                onChange={onChangeEvent}
+                error={errors.githubusername}
+                info="If you want your latest repos and a Github link, include your username."
+              />
+              <TextAreaFieldGroup
+                placeholder="A short bio of yourself"
+                name="bio"
+                value={bio}
+                onChange={onChangeEvent}
+                error={errors.bio}
+                info="Tell us a little about yourself."
+              />
+              <div className="mb-3">
+                <button
+                  onClick={() => {
+                    setDisplaySocialInputs(!displaySocialInputs);
+                  }}
+                  className="btn btn-light"
+                >
+                  Add Social Network Links
+                </button>
+                <span className="text-muted"> Optional</span>
+              </div>
+              {socialInputs}
+              <input
+                type="submit"
+                value="Submit"
+                className="btn btn-info btn-block mt-4"
+              />
+            </form>
           </div>
         </div>
       </div>
